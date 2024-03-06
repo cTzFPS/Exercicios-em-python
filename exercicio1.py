0fcbcd1b498cd47625b6076b3b29ce34ba946c83
Gabriel Matos <103896944+cTzFPS@users.noreply.github.com>
2024-03-06 18:53:58 -0300

    Add files via upload

---
+++ exercicio1.py
@@ -0,0 +1,7 @@
+nome = input('digite o nome do usuário: ')
+v1 = int(input('digite o valor da base do retângulo:'))
+v2 = int(input('digite o valor de um lado do retângulo:'))
+
+print('Os valores digitados foram:')
+print('A área do retângulo é:', (v1 * v2))
+print('O seu perímetro é:' , (v1 + v2) * 2)
---
+++ exercicio2.py
@@ -0,0 +1,5 @@
+nome = input('Qual o seu nome?')
+salario = float(input('E o seu salário?'))
+
+print(nome , 'tem o salário de:', salario)
+print('O seu salário com acréscimo de 5% ficaram em:', salario + (5 * salario) / 100)
